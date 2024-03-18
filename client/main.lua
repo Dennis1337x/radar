1b73853bf6c52e9ac98af2f18be40aa4618fb66e
Dennis1337x <113421270+Dennis1337x@users.noreply.github.com>
2024-03-19 00:05:33 +0300

    Add files via upload

---
+++ client/main.lua
@@ -0,0 +1,12 @@
+addEventHandler("onClientResourceStart", resourceRoot, function ()
+	Radar.start()
+	setRadarVisible(true)
+end)
+
+-- Web Site : https://sparrow-mta.blogspot.com/
+
+-- Facebook : https://facebook.com/sparrowgta/
+-- İnstagram : https://instagram.com/sparrowmta/
+-- YouTube : https://youtube.com/c/SparroWMTA/
+
+-- Discord : https://discord.gg/DzgEcvy
